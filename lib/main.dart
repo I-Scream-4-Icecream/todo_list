9f03b8b20d6caabd758c9ac6013b7290cbee13ae
Remi <remikassouah@gmail.com>
2021-09-08 19:43:18 +0300

    added notifiers file and riverpod dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:todo_list/pages/add.dart';
 import 'package:todo_list/pages/home.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 main() async {
-  //WidgetsFlutterBinding.ensureInitialized();
-  //SystemChrome.setEnabledSystemUIOverlays(SystemUiOverlay.values);
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    routes: {
-      '/': (context) => Home(),
-      '/add': (context) => AddTask(),
-    },
+  runApp(ProviderScope(
+    child: MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => Home(),
+        '/add': (context) => AddTask(),
+      },
+    )
   ));
 }
--- lib/pages/add.dart
+++ lib/pages/add.dart
@@ -11,6 +11,7 @@ class AddTask extends StatelessWidget {
       backgroundColor: Color(0xFF3d47af),
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget> [
             Padding(
               padding: EdgeInsets.only(top: screenHeight*0.18, left: screenWidth*0.09),
@@ -24,7 +25,60 @@ class AddTask extends StatelessWidget {
               ),
             ),
             Padding(
-              padding: EdgeInsets.only(top: screenHeight*0.04, left: screenWidth*0.09),
+              padding: EdgeInsets.only(top: screenHeight*0.06, left: screenWidth*0.09, right: screenWidth*0.09),
+              child: TextField(
+                textAlign: TextAlign.left,
+                style: TextStyle(color: Colors.white),
+                cursorWidth: 1,
+                maxLength: 16,
+                cursorColor: Color(0xFFBBC2D8),
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(13)),
+                    borderSide: BorderSide(color: Color(0xFFBBC2D8))
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(25)),
+                    borderSide: BorderSide(color: Color(0xFFBBC2D8))
+                  ),
+                  hintText: 'Task Title',
+                  hintStyle: TextStyle(color: Color(0xFFBBC2D8), fontWeight: FontWeight.w100),
+                  counterStyle: TextStyle(color: Color(0xFFBBC2D8))
+                ),
+              ),
+            ),
+            Center(
+              child: InkWell(
+                onTap: () {
+                  print(0);
+                },
+                child: Material(
+                  child: Center(
+                    child: Text(
+                      'Add',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  /*height: screenHeight*0.11,
+                  padding: EdgeInsets.only(left: 20, right: 20),*/
+                  /*decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    color: Color(0xFFd103fc),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color(0xFFd103fc).withOpacity(0.3),
+                        spreadRadius: 2,
+                        blurRadius: 8,
+                        offset: Offset(-2, 4), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  width: 210*/
+                ),
+              )
             )
           ],
         ),
