6bc528fae733f65ca04bd4593202652b9c4f302c
Remi <remikassouah@gmail.com>
2021-09-09 00:57:17 +0300

    created decoration file and finished functionality of add.dart

--- lib/pages/add.dart
+++ lib/pages/add.dart
@@ -27,7 +27,7 @@ class AddTodo extends StatelessWidget {
                     onPressed: () {
                       Navigator.pushReplacementNamed(context, '/');
                     },
-                  )
+                  ) 
                 ],
               )
             ),
